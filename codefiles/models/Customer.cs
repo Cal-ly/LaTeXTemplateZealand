e49e6ad8d85320ed9370c08d6e869327d8f843b0
Cal-ly <cal-ly@outlook.dk>
2024-09-01 22:57:47 +0200

    chore: Remove unused code files and update build configuration

--- codefiles/models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HttpWebshopCookie.Models.Users;
-
-public class Customer : ApplicationUser
-{
-    public string? Title { get; set; }
-    public DateTime? BirthDate { get; set; }
-    public virtual Company? Company { get; set; }
-    public string? CompanyId { get; set; }
-}
--- codefiles/models/Employee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HttpWebshopCookie.Models.Users;
-
-public class Employee : ApplicationUser
-{
-    public string? JobTitle { get; set; }
-    public decimal? Salary { get; set; }
-    public DateTime? TerminationDate { get; set; }
-}
