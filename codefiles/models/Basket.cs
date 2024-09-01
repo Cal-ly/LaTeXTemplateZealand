a6dca4a4c936f4467a49aea4fbca5083ae1271e2
Cal-ly <cal-ly@outlook.dk>
2024-09-01 22:28:06 +0200

    Added files for template to showcase

---
+++ codefiles/models/Basket.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpWebshopCookie.Models;
+
+public class Basket
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public ICollection<BasketItem> Items { get; set; } = [];
+    public decimal TotalPrice()
+    {
+        return Items.Sum(item => item.LinePrice());
+    }
+    public int TotalItems()
+    {
+        return Items.Sum(item => item.Quantity) ?? 0;
+    }
+}
---
+++ codefiles/models/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿namespace HttpWebshopCookie.Models;
+
+public class BasketItem
+{
+    public Basket? Basket { get; set; }
+    public string? BasketId { get; set; }
+    public Product? ProductInBasket { get; set; }
+    public string? ProductId { get; set; }
+    public int? Quantity { get; set; }
+    public decimal LinePrice()
+    {
+        return ProductInBasket?.Price * Quantity ?? 0;
+    }
+}
